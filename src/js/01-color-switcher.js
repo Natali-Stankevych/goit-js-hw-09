5ec5046a439efdc38180d4c8ecc82a053e1bfa08
Natali-Stankevych <rusakovanatali94@gmail.com>
2023-05-10 21:22:38 +0300

    add task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
 
+let timerId = null;
+
+
+
+buttonStart.addEventListener('click', onStart);
+buttonStop.addEventListener('click', onStop);
+
+function onStart() {
+    timerId = setInterval(bgColor, 1000);
+    
+    buttonStart.toggleAttribute('disabled');
+}
+
+function onStop() {
+    clearInterval(timerId);
+
+    buttonStart.removeAttribute('disabled');
+}
+
+function bgColor() {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
